e5707dd9331a7dbfccc1ffcc1d5bab6356d96a62
SatvikS7 <satviksriram7@gmail.com>
2025-08-23 17:06:22 -0700

    Init Commit

---
+++ src/test/java/HobbyList/example/HobbyList/HobbyListApplicationTests.java
@@ -0,0 +1,13 @@
+package HobbyList.example.HobbyList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
