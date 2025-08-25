eba70f0d3954ce1257fa1e212d57a1dc47caf937
SatvikS7 <satviksriram7@gmail.com>
2025-08-24 19:42:54 -0700

    added basic pages and routing

---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,20 @@
+function SignupPage() {
+return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <label>
+          Email:
+          <input type="email" name="email" required />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" required />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignupPage;
